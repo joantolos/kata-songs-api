4f21f3a24e4a0197e2f48cb9f0bbfcce5350e6e8
joan.tolos <joan.tolos@clarivate.com>
2020-01-05 15:20:01 +0100

    Including header to feign interface!

--- kata-songs-api-src/src/main/java/com/joantolos/kata/songs/api/remote/CountryAPIClient.java
+++ kata-songs-api-src/src/main/java/com/joantolos/kata/songs/api/remote/CountryAPIClient.java
@@ -1,6 +1,7 @@
 package com.joantolos.kata.songs.api.remote;
 
 import com.joantolos.kata.songs.api.domain.entity.RetrieveOutput;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient("country-songs-api")
 public interface CountryAPIClient {
 
+    @Headers("include-remote:false")
     @RequestMapping(method = RequestMethod.GET, value = "/song/all")
     RetrieveOutput getAllSongs();
 }
